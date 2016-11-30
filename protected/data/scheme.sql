eb590706eed064362b5599f844ba7fb301338db4
tmazza <tiagomdepaula@gmail.com>
2016-11-29 22:41:51 -0200

    scheme atualizado

--- protected/data/scheme.sql
+++ protected/data/scheme.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2016 at 12:05 AM
+-- Generation Time: Nov 30, 2016 at 12:41 AM
 -- Server version: 5.5.43-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.17
 
@@ -40,6 +40,20 @@ CREATE TABLE IF NOT EXISTS `item` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `item_troca`
+--
+
+CREATE TABLE IF NOT EXISTS `item_troca` (
+  `troca_id` int(11) NOT NULL,
+  `itemParaTroca_id` int(11) NOT NULL,
+  `tipo` int(1) NOT NULL,
+  PRIMARY KEY (`troca_id`,`itemParaTroca_id`),
+  KEY `fK_troca_itemSolicitado_ipt` (`itemParaTroca_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `item_usuario`
 --
 
@@ -53,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `item_usuario` (
   PRIMARY KEY (`id`),
   KEY `fk_item_usuario_user` (`usuario_id`),
   KEY `fx_item_usuario_item` (`item_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=92 ;
 
 -- --------------------------------------------------------
 
@@ -69,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `mensagem` (
   PRIMARY KEY (`id`),
   KEY `fx_mensagem_usuario` (`remetente_id`),
   KEY `fx_mensagem_troca` (`troca_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -91,33 +105,7 @@ CREATE TABLE IF NOT EXISTS `troca` (
   PRIMARY KEY (`id`),
   KEY `fk_troca_solicitante` (`usuarioSolicitante_id`),
   KEY `fk_troca_quedecide` (`usuarioQueDecide_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `troca_itemOferecido`
---
-
-CREATE TABLE IF NOT EXISTS `troca_itemOferecido` (
-  `troca_id` int(11) NOT NULL,
-  `itemParaTroca_id` int(11) NOT NULL,
-  PRIMARY KEY (`troca_id`,`itemParaTroca_id`),
-  KEY `fK_troca_itemOferecido_ipt` (`itemParaTroca_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `troca_itemSolicitado`
---
-
-CREATE TABLE IF NOT EXISTS `troca_itemSolicitado` (
-  `troca_id` int(11) NOT NULL,
-  `itemParaTroca_id` int(11) NOT NULL,
-  PRIMARY KEY (`troca_id`,`itemParaTroca_id`),
-  KEY `fK_troca_itemSolicitado_ipt` (`itemParaTroca_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 -- --------------------------------------------------------
 
@@ -134,11 +122,18 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `qtdTrocaRealizadas` int(11) NOT NULL DEFAULT '0',
   `somaAvaliacoes` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=55 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=60 ;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `item_troca`
+--
+ALTER TABLE `item_troca`
+  ADD CONSTRAINT `fk_item_troca_item` FOREIGN KEY (`itemParaTroca_id`) REFERENCES `item_usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_item_troca_troca` FOREIGN KEY (`troca_id`) REFERENCES `troca` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `mensagem`
@@ -153,18 +148,6 @@ ALTER TABLE `mensagem`
 ALTER TABLE `troca`
   ADD CONSTRAINT `fk_troca_quedecide` FOREIGN KEY (`usuarioQueDecide_id`) REFERENCES `usuario` (`id`),
   ADD CONSTRAINT `fk_troca_solicitante` FOREIGN KEY (`usuarioSolicitante_id`) REFERENCES `usuario` (`id`);
-
---
--- Constraints for table `troca_itemOferecido`
---
-ALTER TABLE `troca_itemOferecido`
-  ADD CONSTRAINT `fK_troca_itemOferecido_troca` FOREIGN KEY (`troca_id`) REFERENCES `troca` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `troca_itemSolicitado`
---
-ALTER TABLE `troca_itemSolicitado`
-  ADD CONSTRAINT `fK_troca_itemSolicitado_troca` FOREIGN KEY (`troca_id`) REFERENCES `troca` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
